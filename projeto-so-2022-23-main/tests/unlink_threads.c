9504cb5cc4621ea6341559aec2c91f6d73e3a4a7
Diogo Correia <diogo.filipe.correia@tecnico.ulisboa.pt>
2022-12-20 21:38:43 +0000

    asd

--- projeto-so-2022-23-main/tests/unlink_threads.c
+++ projeto-so-2022-23-main/tests/unlink_threads.c
@@ -1,15 +1,14 @@
-#include "fs/operations.h"
+#include "../fs/operations.h"
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <stdlib.h>
-
 
 #define THREAD_COUNT 15
 #define MAX_LEN_FILE_NAME 10
@@ -24,8 +23,14 @@ int main() {
     int table[THREAD_COUNT];
     table[0] = 0;
 
+    char target[MAX_FILE_NAME] = {"/f1"};
+    int f = tfs_open(target, TFS_O_CREAT);
+    assert(f != -1);
+
+    assert(tfs_close(f) != -1);
+
     for (int i = 1; i < THREAD_COUNT; ++i) {
-        switch(i % 2) {
+        switch (i % 2) {
         case 0:
             pthread_create(&tid[i], NULL, unlink_hardlink, &table[i]);
             break;
@@ -43,35 +48,33 @@ int main() {
         pthread_join(tid[i], NULL);
     }
 
-    printf("Successful tes\n");
+    printf("Successful test\n");
 
     return 0;
 }
 
 void *unlink_hardlink(void *arg) {
-    (void)arg;
+    int file_i = *((int *)arg);
 
     char target[MAX_FILE_NAME] = {"/f1"};
 
-    char link_name[MAX_FILE_NAME] = {"/l1"};
+    char link_name[MAX_FILE_NAME] = {"/l"};
+    sprintf(link_name + 2, "%d", file_i);
 
-    assert(tfs_link(target, link_name) == 0);
-
-    assert(unlink(target) == 0);
+    assert(tfs_link(target, link_name) != -1);
 
     return NULL;
 }
 
 void *unlink_symlink(void *arg) {
-    (void)arg;
+    int file_i = *((int *)arg);
 
     char target[MAX_FILE_NAME] = {"/f1"};
 
-    char link_name[MAX_FILE_NAME] = {"/l1"};
+    char link_name[MAX_FILE_NAME] = {"/l"};
+    sprintf(link_name + 2, "%d", file_i);
 
-    assert(tfs_sym_link(target, link_name) == 0);
-
-    assert(unlink(target) == 0);
+    assert(tfs_sym_link(target, link_name) != -1);
 
     return NULL;
 }
