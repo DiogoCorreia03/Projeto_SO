f86a602ef15944a3f3fcf1706926d89b1080204f
Diogo Correia <diogo.filipe.correia@tecnico.ulisboa.pt>
2023-01-11 12:54:04 +0000

    queue

--- projeto-so-2022-23-main/producer-consumer/producer-consumer.c
+++ projeto-so-2022-23-main/producer-consumer/producer-consumer.c
@@ -1,4 +1,6 @@
 #include "producer-consumer.h"
+#include "../utils/logging.h"
+#include <errno.h>
 #include <pthread.h>
 #include <stdlib.h>
 
@@ -9,20 +11,41 @@ int pcq_create(pc_queue_t *queue, size_t capacity) {
         return -1;
     }
 
-    // Initialize the queue properties
+    // Initialize the queue variables
     queue->pcq_capacity = capacity;
     queue->pcq_current_size = 0;
     queue->pcq_head = 0;
     queue->pcq_tail = 0;
 
-    // Initialize the locks and condition variables
-    pthread_mutex_init(&queue->pcq_current_size_lock, NULL);
-    pthread_mutex_init(&queue->pcq_head_lock, NULL);
-    pthread_mutex_init(&queue->pcq_tail_lock, NULL);
-    pthread_mutex_init(&queue->pcq_pusher_condvar_lock, NULL);
-    pthread_cond_init(&queue->pcq_pusher_condvar, NULL);
-    pthread_mutex_init(&queue->pcq_popper_condvar_lock, NULL);
-    pthread_cond_init(&queue->pcq_popper_condvar, NULL);
+    // Initialize the condition variables and locks
+    if (pthread_mutex_init(&queue->pcq_current_size_lock, NULL) == -1) {
+        // WARN("failed to init mutex: %s", strerror(errno));
+        return -1;
+    }
+    if (pthread_mutex_init(&queue->pcq_head_lock, NULL) == -1) {
+        // WARN("failed to init mutex: %s", strerror(errno));
+        return -1;
+    }
+    if (pthread_mutex_init(&queue->pcq_tail_lock, NULL) == -1) {
+        // WARN("failed to init mutex: %s", strerror(errno));
+        return -1;
+    }
+    if (pthread_mutex_init(&queue->pcq_pusher_condvar_lock, NULL) == -1) {
+        // WARN("failed to init mutex: %s", strerror(errno));
+        return -1;
+    }
+    if (pthread_cond_init(&queue->pcq_pusher_condvar, NULL) == -1) {
+        // WARN("failed to init cond: %s", strerror(errno));
+        return -1;
+    }
+    if (pthread_mutex_init(&queue->pcq_popper_condvar_lock, NULL) == -1) {
+        // WARN("failed to init mutex: %s", strerror(errno));
+        return -1;
+    }
+    if (pthread_cond_init(&queue->pcq_popper_condvar, NULL) == -1) {
+        // WARN("failed to init cond: %s", strerror(errno));
+        return -1;
+    }
 
     return 0;
 }
@@ -32,35 +55,68 @@ int pcq_destroy(pc_queue_t *queue) {
     free(queue->pcq_buffer);
 
     // Destroy the locks and condition variables
-    pthread_mutex_destroy(&queue->pcq_current_size_lock);
-    pthread_mutex_destroy(&queue->pcq_head_lock);
-    pthread_mutex_destroy(&queue->pcq_tail_lock);
-    pthread_mutex_destroy(&queue->pcq_pusher_condvar_lock);
-    pthread_cond_destroy(&queue->pcq_pusher_condvar);
-    pthread_mutex_destroy(&queue->pcq_popper_condvar_lock);
-    pthread_cond_destroy(&queue->pcq_popper_condvar);
+    if (pthread_mutex_destroy(&queue->pcq_current_size_lock) == -1) {
+        // WARN("failed to destroy mutex: %s", strerror(errno));
+        return -1;
+    }
+    if (pthread_mutex_destroy(&queue->pcq_head_lock) == -1) {
+        // WARN("failed to destroy mutex: %s", strerror(errno));
+        return -1;
+    }
+    if (pthread_mutex_destroy(&queue->pcq_tail_lock) == -1) {
+        // WARN("failed to destroy mutex: %s", strerror(errno));
+        return -1;
+    }
+    if (pthread_mutex_destroy(&queue->pcq_pusher_condvar_lock) == -1) {
+        // WARN("failed to destroy mutex: %s", strerror(errno));
+        return -1;
+    }
+    if (pthread_cond_destroy(&queue->pcq_pusher_condvar) == -1) {
+        // WARN("failed to destroy cond: %s", strerror(errno));
+        return -1;
+    }
+    if (pthread_mutex_destroy(&queue->pcq_popper_condvar_lock) == -1) {
+        // WARN("failed to destroy mutex: %s", strerror(errno));
+        return -1;
+    }
+    if (pthread_cond_destroy(&queue->pcq_popper_condvar) == -1) {
+        // WARN("failed to destroy cond: %s", strerror(errno));
+        return -1;
+    }
 
     return 0;
 }
 
 int pcq_enqueue(pc_queue_t *queue, void *elem) {
     // Acquire the lock on the current size
-    pthread_mutex_lock(&queue->pcq_current_size_lock);
+    if (pthread_mutex_lock(&queue->pcq_current_size_lock) == -1) {
+        // WARN("failed to lock mutex: %s", strerror(errno));
+        return -1;
+    }
 
     // Wait until the queue has space
     while (queue->pcq_current_size == queue->pcq_capacity) {
-        pthread_cond_wait(&queue->pcq_pusher_condvar,
-                          &queue->pcq_current_size_lock);
+        if (pthread_cond_wait(&queue->pcq_pusher_condvar,
+                              &queue->pcq_current_size_lock) == -1) {
+            // WARN("failed to wait cond: %s", strerror(errno));
+            return -1;
+        }
     }
 
     // Increment the current size
     queue->pcq_current_size++;
 
     // Release the lock on the current size
-    pthread_mutex_unlock(&queue->pcq_current_size_lock);
+    if (pthread_mutex_unlock(&queue->pcq_current_size_lock) == -1) {
+        // WARN("failed to unlock mutex: %s", strerror(errno));
+        return -1;
+    }
 
     // Acquire the lock on the head
-    pthread_mutex_lock(&queue->pcq_head_lock);
+    if (pthread_mutex_lock(&queue->pcq_head_lock) == -1) {
+        // WARN("failed to lock mutex: %s", strerror(errno));
+        return -1;
+    }
 
     // Insert the new element at the head of the buffer
     queue->pcq_buffer[queue->pcq_head] = elem;
@@ -69,10 +125,16 @@ int pcq_enqueue(pc_queue_t *queue, void *elem) {
     queue->pcq_head = (queue->pcq_head + 1) % queue->pcq_capacity;
 
     // Release the lock on the head
-    pthread_mutex_unlock(&queue->pcq_head_lock);
+    if (pthread_mutex_unlock(&queue->pcq_head_lock) == -1) {
+        // WARN("failed to unlock mutex: %s", strerror(errno));
+        return -1;
+    }
 
     // Wake up the popper thread
-    pthread_cond_signal(&queue->pcq_popper_condvar);
+    if (pthread_cond_signal(&queue->pcq_popper_condvar) == -1) {
+        // WARN("failed to wake cond: %s", strerror(errno));
+        return -1;
+    }
 
     return 0;
 }
@@ -81,22 +143,34 @@ void *pcq_dequeue(pc_queue_t *queue) {
     void *elem;
 
     // Acquire the lock on the current size
-    pthread_mutex_lock(&queue->pcq_current_size_lock);
+    if (pthread_mutex_lock(&queue->pcq_current_size_lock) == -1) {
+        // WARN("failed to lock mutex: %s", strerror(errno));
+        return -1;
+    }
 
     // Wait until the queue has an element
     while (queue->pcq_current_size == 0) {
-        pthread_cond_wait(&queue->pcq_popper_condvar,
-                          &queue->pcq_current_size_lock);
+        if (pthread_cond_wait(&queue->pcq_popper_condvar,
+                              &queue->pcq_current_size_lock) == -1) {
+            // WARN("failed to wait cond: %s", strerror(errno));
+            return -1;
+        }
     }
 
     // Decrement the current size
     queue->pcq_current_size--;
 
     // Release the lock on the current size
-    pthread_mutex_unlock(&queue->pcq_current_size_lock);
+    if (pthread_mutex_unlock(&queue->pcq_current_size_lock) == -1) {
+        // WARN("failed to unlock mutex: %s", strerror(errno));
+        return -1;
+    }
 
     // Acquire the lock on the tail
-    pthread_mutex_lock(&queue->pcq_tail_lock);
+    if (pthread_mutex_lock(&queue->pcq_tail_lock) == -1) {
+        // WARN("failed to lock mutex: %s", strerror(errno));
+        return -1;
+    }
 
     // Remove the element from the tail of the buffer
     elem = queue->pcq_buffer[queue->pcq_tail];
@@ -105,10 +179,16 @@ void *pcq_dequeue(pc_queue_t *queue) {
     queue->pcq_tail = (queue->pcq_tail + 1) % queue->pcq_capacity;
 
     // Release the lock on the tail
-    pthread_mutex_unlock(&queue->pcq_tail_lock);
+    if (pthread_mutex_unlock(&queue->pcq_tail_lock) == -1) {
+        // WARN("failed to unlock mutex: %s", strerror(errno));
+        return -1;
+    }
 
     // Wake up the pusher thread
-    pthread_cond_signal(&queue->pcq_pusher_condvar);
+    if (pthread_cond_signal(&queue->pcq_pusher_condvar) == -1) {
+        // WARN("failed to wake cond: %s", strerror(errno));
+        return -1;
+    }
 
     return elem;
 }
